4b7a7db5df3dbf1a04de8b6ed38644f3ca2334c5
Jon Seager <jonseager@gmail.com>
2016-12-12 20:50:25 +0000

    Added config file example, requirements file for pip and merged the plot/parse scripts into the main wigle_query.py script.

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
 ## Basic Wigle API Script
 
-Quick and dirty, `wigle_query.py` queries Wigle.net and gets all Wifi hotspots known for the specified location in JSON format.
+Wigle Python script that can search a specific area for hotspots and output the results as JSON, CSV or KML.
 
-This JSON file can then be converted into a CSV file using `parse_wigle.py`
+To install/set up:
+
+```
+git clone ssh://git@git.jon0.co.uk:2222/jon/pywigle.git
+cd pywigle
+pip install -r requirements.txt
+mv config.json.example config.json
+```
+
+Next, edit `config.json` and add your Wigle API key details. These can be found [here](https://wigle.net/account) (Wigle.net account required).
+
+```
+usage: wigle_query.py [-h] [--location LOCATION] [--json-out JSON_OUT]
+                      [--kml-out KML_OUT] [--csv-out CSV_OUT]
+                      [--input-json INPUT_JSON] [--print]
+
+Searches Wigle.net for all Wifi hotspots that have been seen in a specified
+area
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --location LOCATION, -l LOCATION
+                        Location to query. E.g. "Basingstoke, Hampshire, UK"
+  --json-out JSON_OUT, -j JSON_OUT
+                        Name of JSON file to write if required.
+  --kml-out KML_OUT, -k KML_OUT
+                        Name of KML file to write if required.
+  --csv-out CSV_OUT, -c CSV_OUT
+                        Name of CSV file to write if required.
+  --input-json INPUT_JSON, -i INPUT_JSON
+                        Name of JSON file to read from. Used to convert
+                        existing JSON file into CSV/KML.
+  --print, -p           Output JSON to stdout.
+  ```
--- wigle_query.py
+++ wigle_query.py
@@ -1,23 +1,20 @@
 import argparse
 import json
 import requests
+import csv
+from pykml.factory import KML_ElementMaker as KML
+from lxml import etree
 
 class Wigle:
-    """
-    Wigle class should be instantiated with wigle API Name and API Token which can be accessed from https://wigle.net/account
-    """
+    
     api_name  = None
     api_token = None
+    json_data = None
 
-    def __init__(self, api_name, api_token):
-        """
-        Wigle Constructor
-
-        :param api_name:    String with API Name
-        :param api_token:   String with API Token
-        """
-        self.api_name = api_name
-        self.api_token = api_token
+    def __init__(self):
+        data = json.loads(open('config.json').read())
+        self.api_name = data['api_creds']['api_name']
+        self.api_token = data['api_creds']['api_token']
 
     def network_search(self, address):
         """
@@ -38,56 +35,115 @@ class Wigle:
                         "latrange1" : box[0],
                         "latrange2" : box[1],
                         "longrange1" : box[2],
-                        "longrange2" : box[3],
-                        "freenet" : "false",
-                        "paynet" : "false"
+                        "longrange2" : box[3]
                     }
                 )
 
-            if response.status_code == 200:
-                response_json = json.loads(response.text)
-                if response_json.get("error"):
-                    print "network_search: " + response_json.get("error")
-                    exit()
+            try:
+                if response_json.get("resultCount") > 0:
+                    results += response_json.get("results")
+                    first += 100
                 else:
-                    if response_json.get("resultCount") > 0:
-                        results += response_json.get("results")
-                        first += 100
-                    else:
-                        break
-            else:
-                print "network_search: " + response.status_code
+                    break
+            except NameError:
+                print "get_box: Unexpected response. Have you hit your query limit?"
                 exit()
-
+    
+        self.json_data = results
         return results
 
-    def get_box(self, address):
-        response = requests.get(
-                url = "https://api.wigle.net/api/v2/network/geocode",
-                auth = (self.api_name,  self.api_token),
-                params = { "addresscode" : address }
-            )
+    def import_json(self, filename):
+        data = open(filename).read()
+        self.json_data = json.loads(data.decode('unicode_escape').encode('ascii','ignore'))
 
-        if response.status_code == 200:
-            response_json = json.loads(response.text)
-            if response_json.get("error"):
-                print "get_box: " + response_json.get("error")
-                exit()
-            else:
-                return response_json.get("results")[0]["boundingbox"]
-        else:
-            print "get_box: " + response.status_code
+    def export_kml(self, filename):
+        try:
+            document = KML.kml(KML.Document())
+            document.Document.append(
+                KML.Style(
+                    KML.IconStyle(
+                        KML.scale(1.0),
+                        KML.color('ff0000ff'),
+                        KML.Icon(
+                            KML.href("http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png"),
+                        ),
+                        id="mystyle"
+                    ),
+                    id="circle"
+                    )
+                )
+
+            for net in self.json_data:
+                pm = KML.Placemark(
+                    KML.name(net["ssid"]),
+                    KML.styleUrl("#circle"),
+                    KML.Point(KML.coordinates(net["trilong"],',',net["trilat"])),
+                    KML.ExtendedData(
+                        KML.Data(KML.value(net["ssid"]), name="SSID"),
+                        KML.Data(KML.value(net["channel"]), name="Channel"),
+                        KML.Data(KML.value(net["wep"]), name="Encryption"),
+                        KML.Data(KML.value(net["type"]), name="Type"),
+                        KML.Data(KML.value(net["netid"]), name="BSSID"),
+                        KML.Data(KML.value(net["firsttime"]), name="First Seen"),
+                        KML.Data(KML.value(net["lastupdt"]), name="Last Updated")
+                    )
+                )
+                document.Document.append(pm)
+
+            outfile = open(filename, 'w')
+            outfile.write(etree.tostring(document, pretty_print=True))
+            outfile.close()
+        except TypeError:
+            print "Export to KML: No data to export!"
+
+    def export_csv(self, filename):
+        try:
+            outfile = open(filename,'w')
+            csvwriter = csv.writer(outfile)
+            for idx,net in enumerate(self.json_data):
+                if idx == 0: csvwriter.writerow(net.keys())
+                csvwriter.writerow(net.values())
+            outfile.close()
+        except TypeError:
+            print "Export to CSV: No data to export!"
             exit()
 
+    def export_json(self, filename):
+        try:
+            outfile = open(filename,'w')
+            json.dump(self.json_data, outfile)
+            outfile.close()
+        except TypeError:
+            print "Export to JSON: No data to export!"
+            exit()
 
-    def pp(self, json_stuff):
-        print json.dumps(json_stuff, sort_keys=True, indent=4)
+    def get_box(self, address):
+        response = requests.get(url = "https://api.wigle.net/api/v2/network/geocode", auth = (self.api_name,  self.api_token), params = { "addresscode" : address })
+        try:
+            return response_json.get("results")[0]["boundingbox"]
+        except NameError:
+            print "get_box: Unexpected response. Have you hit your query limit?"
+            exit()        
+
+    def pp(self):
+        print json.dumps(self.json_data, sort_keys=True, indent=4)
 
 
-parser = argparse.ArgumentParser(description='Searches Wigle.net for all Wifi hotspots that have been seen in a specified area')
-parser.add_argument('--location', '-l', required=True, type=str, help='Location to query. E.g. "Basingstoke, Hampshire, UK"')
-args = vars(parser.parse_args())
+parser = argparse.ArgumentParser(description='Searches Wigle.net for all Wifi hotspots that have been seen in a specified area. If no output formats are specified (JSON/CSV/KML), the script will output JSON to stdout')
+parser.add_argument('--location', '-l', required=False, type=str, help='Location to query. E.g. "Basingstoke, Hampshire, UK"')
+parser.add_argument('--json-out', '-j', required=False, type=str, help='Name of JSON file to write if required.')
+parser.add_argument('--kml-out', '-k', required=False, type=str, help='Name of KML file to write if required.')
+parser.add_argument('--csv-out', '-c', required=False, type=str, help='Name of CSV file to write if required.')
+parser.add_argument('--input-json', '-i', required=False, type=str, help='Name of JSON file to read from. Used to convert existing JSON file into CSV/KML.')
+parser.add_argument('--print', '-p', required=False, action='store_true', help='Output JSON to stdout.')
+args = parser.parse_args()
 
+wigle = Wigle()
+if args.location: wigle.network_search(args.location)
+if args.input_json: wigle.import_json(args.input_json)
 
-wigle = Wigle("AIDf013644d91eea7d64d26593fb44bb4fb", "e3f1986ddaac1e1eec6a466ce200a183")
-wigle.pp(wigle.network_search(args["location"]))
+if args.json_out: wigle.export_json(args.json_out)
+if args.csv_out: wigle.export_csv(args.csv_out)
+if args.kml_out: wigle.export_kml(args.kml_out)
+
+if (not(args.json_out or args.kml_out or args.csv_out)) or getattr(args, 'print') : wigle.pp()
